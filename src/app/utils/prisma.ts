64c87f349bac7a992fc05b491486d9fb2c436c82
arFaisal043 <arfaisal463@gmail.com>
2026-07-31 19:06:44 +0600

    Fix: Configure Prisma 7 driver adapter

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import config from '../config';
 
-const prisma = new PrismaClient();
+const pool = new Pool({ connectionString: config.database_url });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
